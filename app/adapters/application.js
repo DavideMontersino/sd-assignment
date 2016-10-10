fd495e671372ae1c0990b4395b998339a553f29b
Davide Montersino <davide@weborama.nl>
2016-10-10 09:53:39 +0200

    chore: remove unused deps
    remove bearer

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -6,6 +6,6 @@ export default DS.JSONAPIAdapter.extend({
   headers: {
     // TODO this should be server side, on a proxy service or similar,
     // and we should have an auth mechanism (i.e.: cookie based or JWT)
-    Authorization: 'Bearer 8aef9517-3070-4090-b55e-83296cee8cd1'
+    Authorization: 'Bearer'
   }
 });
